3bd9723cccd87eb4dd3fb6e8739fbcd35cf6bdbf
Manthan Bire <manthansharadbire@gmail.com>
2025-03-23 11:25:23 +0530

    initial searching design has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Search} from 'lucide-react';
 
 function App() {
   return (
@@ -6,13 +7,22 @@ function App() {
     <div>
     <h1 className='text-3xl font-semibold text-center m-10'>TODO LIST</h1>
     </div>
-    <div>
+    <div className='flex justify-center'>
     <input type="text" 
     placeholder='Search note here' 
     className='border border-gray-400
     outline-none
     m-auto block
-    py-4 px-4 w-1/3'/>  
+    py-4 px-4 w-1/2
+    rounded-lg shadow-lg'/>
+    <div>
+    <button className='border
+     border-gray-300 
+     absolute right-60 
+     px-9 p-4 bg-blue-400
+     text-white rounded-lg shadow-lg'>
+    Search</button>
+    </div>
     </div>
     </div>
   )
