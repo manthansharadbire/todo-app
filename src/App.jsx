de36337dec56266d3e818e06aee3668ec78631bb
Manthan Bire <manthansharadbire@gmail.com>
2025-03-23 10:04:20 +0530

    search box added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function App() {
   return (
     <div>
-      
+    <div>
+    <h1 className='text-3xl font-semibold text-center m-10'>TODO LIST</h1>
+    </div>
+    <div>
+    <input type="text" 
+    placeholder='Search note here' 
+    className='border border-gray-400
+    outline-none
+    m-auto block
+    py-4 px-4 w-1/3'/>  
+    </div>
     </div>
   )
 }
