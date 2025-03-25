d55ecb6d6e3de4571307cd6f3ad2ea58bc09e4c0
Manthan Bire <manthansharadbire@gmail.com>
2025-03-25 15:52:41 +0530

    priority selector, input box & add button has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
-import {Search} from 'lucide-react';
 
 function App() {
   return (
     <div>
     <div>
-    <h1 className='text-3xl font-semibold text-center m-10'>TODO LIST</h1>
+    <h1 className='text-3xl font-semibold text-center m-5'>TODO LIST</h1>
     </div>
     <div className='flex justify-center'>
     <input type="text" 
-    placeholder='Search note here' 
-    className='border border-gray-400
+    placeholder='Add your task or note here' 
+    className='border
+     border-gray-400
     outline-none
-    m-auto block
-    py-4 px-4 w-1/2
-    rounded-lg shadow-lg'/>
-    <div>
-    <button className='border
-     border-gray-300 
-     absolute right-60 
-     px-9 p-4 bg-blue-400
-     text-white rounded-lg shadow-lg'>
-    Search</button>
+    w-1/2
+    py-4 px-4 
+    rounded-lg shadow-lg
+    '/>
+    <select className='ml-5 border 
+     border-gray-400
+    outline-none
+    rounded-lg shadow-lg
+    px-4 pr-2'>
+    <option value="High">High</option>
+    <option value="Medium">Medium</option>
+    <option value="Low">Low</option>
+    </select>
+   <button className='border ml-5 px-8
+    border-gray-400
+    outline-none
+    rounded-lg shadow-lg
+    bg-blue-400
+    hover:bg-blue-500'>
+    Add</button>
     </div>
     </div>
-    </div>
+    
   )
 }
 
-export default App
+export default App;
